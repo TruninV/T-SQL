794b55e41bc67430f0e6d823c43fc931693e845f
Виталий Трунин <58661395+TruninV@users.noreply.github.com>
2019-12-08 18:48:50 +0300

    Функция возвращает сумму с учетом НДС

--- 004_Сумма_с_НДС.sql
+++ 004_Сумма_с_НДС.sql
@@ -2,24 +2,24 @@
 CREATE FUNCTION SummaWithNDS
 (
 	@Summa FLOAT, --Сумма
-	@Nds INT 	  --Значение НДС
+	@Nds INT      --Значение НДС
 )
 RETURNS FLOAT
 AS
 BEGIN	
 /*
 	***** Описание
-		  Функция возвращает сумму с учетом НДС
+	      Функция возвращает сумму с учетом НДС
 
 	***** Пример запуска
-		  SELECT dbo.SummaWithNDS(100, 20) AS SummaWithNDS;
+	      SELECT dbo.SummaWithNDS(100, 20) AS SummaWithNDS;
 				  
 	***** Сайт - https://info-comp.ru
 	      GitHub - https://github.com/TruninV/T-SQL
 		  
-	*****Материалы для изучения T-SQL
-		 https://info-comp.ru/t-sql-book.html
-		 https://info-comp.ru/microsoft-sql-server
+	***** Материалы для изучения T-SQL
+	      https://info-comp.ru/t-sql-book.html
+	      https://info-comp.ru/microsoft-sql-server
 */	
 	DECLARE @SummaWithNDS FLOAT;
 	--Если значение НДС отрицательно, то возвращается просто сумма
