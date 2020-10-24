c5cd361263307b2c2df98e3dc65d18053eaaac5d
Виталий Трунин <58661395+TruninV@users.noreply.github.com>
2020-10-24 09:25:41 +0300

    Генерация последовательности дат (способ 2)

---
+++ 009_Функция_для_генерации_последовательности_дат_WITH.sql
@@ -0,0 +1,50 @@
+--Табличная функция для генерации последовательности дат (способ 2 – WITH)
+CREATE FUNCTION GeneratingDates (
+	@DateStart DATE, -- Дата начала
+	@DateEnd DATE	 -- Дата окончания
+)
+RETURNS @ListDates TABLE (dt DATE) 
+AS
+BEGIN
+	/*
+		***** Описание
+			  Генерация последовательности дат в заданном диапазоне.
+			  Способ подразумевает использование рекурсивного обобщенного табличного выражения.
+		
+		***** Функция возвращает таблицу с датами в диапазоне, который задан в параметрах.
+			  Даты формируются начиная @DateStart и заканчивая @DateEnd.
+			  
+		***** Пример запуска
+			  SELECT * FROM GeneratingDates('01.01.2020','31.12.2020');
+			  
+		***** Сайт - https://info-comp.ru
+			  GitHub - https://github.com/TruninV/T-SQL
+			  
+		***** Материалы для изучения T-SQL
+			  https://self-learning.ru/databases
+			  https://info-comp.ru/microsoft-sql-server
+			
+		***** Курсы по T-SQL 
+			  https://self-learning.ru/courses/t-sql
+	*/
+	--Рекурсивное обобщенное табличное выражение.
+	WITH Dates AS
+	(
+		SELECT @DateStart AS DateStart -- Задаем якорь рекурсии
+	
+		UNION ALL
+
+		SELECT DATEADD(DAY, 1, DateStart) AS DateStart -- Увеличиваем значение даты на 1 день
+		FROM Dates
+		WHERE DateStart < @DateEnd -- Прекращаем выполнение, когда дойдем до даты окончания
+	)
+	INSERT INTO @ListDates
+		SELECT DateStart 
+		FROM Dates
+		OPTION (MAXRECURSION 0); 
+		/*
+			Значением 0 снимаем серверное ограничение на количество уровней рекурсии (которое по умолчанию равно 100), 
+			чтобы иметь возможность формировать даты в большом диапазоне.
+		*/
+  RETURN
+END
